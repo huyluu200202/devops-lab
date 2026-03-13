65b5776600d8782dcd9d2e13bbb39d57acda1d36
huyluu20002 <huyluu20002@gmail.com>
2026-03-13 08:14:43 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res)=>{
+  res.send("DevOps Lab Running 🚀")
+})
+
+app.get('/health',(req,res)=>{
+  res.json({status:"ok"})
+})
+
+app.listen(3000,()=>{
+  console.log("Server running on 3000")
+})
